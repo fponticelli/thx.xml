a34972c35840758a327fc98379ec50edaa1409c4
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-10-12 16:27:01 -0600

    draft of XMLWriter

--- src/thx/xml/io/XMLWriter.hx
+++ src/thx/xml/io/XMLWriter.hx
@@ -1,5 +1,45 @@
 package thx.xml.io;
 
+import haxe.io.Output;
+import thx.xml.*;
+
 class XMLWriter {
-  public function new
+  public static function nodeToString(node : Node) : String {
+    var buffer = haxe.io.;
+    var writer = new XMLWriter(buffer);
+    return buffer.toString();
+  }
+
+  var stream : Output;
+  public function new(stream : Output) {
+    this.stream = stream;
+  }
+
+  public function writeCharacterData(cd : CharacterData) {
+
+  }
+
+  public function writeComment(comment : Comment) {
+
+  }
+
+  public function writeDocument(doc : Document) {
+
+  }
+
+  public function writeDocumentType(docType : DocumentType) {
+
+  }
+
+  public function writeElement(el : Element) {
+
+  }
+
+  public function writeNode(node : Node) {
+
+  }
+
+  public function writeText(text : Text) {
+
+  }
 }
